385c373b675ec26514a6962c9004299a441c222a
osvaldo312410 <osvaldo.ti.barbosa@gmail.com>
2025-11-11 10:26:02 -0300

    Mudança de porta para 8081.

--- app.py
+++ app.py
@@ -120,5 +120,5 @@ def api_ftp_download():
         return jsonify({'success': False, 'error': result})
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))
+    port = int(os.environ.get('PORT', 8081))
     app.run(host='0.0.0.0', port=port, debug=False)
